7141a9defd6b93291d07d2c8d643b18c153891ef
reiner-local-mac <matthewhaneburger@gmail.com>
2025-03-27 18:11:46 -0400

    updated App.tsx/main.tsx - need to add publishable key

--- clerk-react/src/main.tsx
+++ clerk-react/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ClerkProvider } from '@clerk/clerk-react'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -12,6 +13,8 @@ if (!PUBLISHABLE_KEY) {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <App />
+    </ClerkProvider>
   </React.StrictMode>,
 )
